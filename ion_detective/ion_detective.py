90b079f121f397990249103c0cc21e1c3b807166
Dom1L <lemm.d@web.de>
2020-04-22 09:40:43 +0200

    acetate and carbonate test, 2/15

--- ion_detective/ion_detective.py
+++ ion_detective/ion_detective.py
@@ -1,13 +1,13 @@
 import random
 
-from ion_detective.utils import anions, cations
+from ion_detective.utils import anions, cations, decision
 
 
 class IonDetective:
     def __init__(self):
-        pass
+        self._ion_probe = None
 
-    def generate_probe(self, ion_type, seed):
+    def generate_probe(self, ion_type: str, seed: int):
         random.seed(seed)
         if ion_type == 'anion':
             ion_list = anions
@@ -16,17 +16,47 @@ class IonDetective:
         else:
             raise ValueError('Know your ion type!')
         # should be encrypted somehow
-        self.ion_probe = random.sample(ion_list, k=3)
+        self._ion_probe = random.sample(ion_list, k=3)
 
-    def get_answers(self, your_solutions):
-        pass
+    def get_answers(self, your_solutions: list):
+        points = 0
+        for ion in your_solutions:
+            if ion in self._ion_probe:
+                points += 1
+        print(f'You achieved {points} out of 3 points!')
 
     # Anion Test
     def test_acetate(self):
-        pass
+        print('A spatula tip of the substance has to be rubbed into the mortar with KHSO4.')
+
+        value = input('How much KHSO4 do you want to add? x times more than substance: x?')
+        if int(value) < 4:
+            return print('You smell nothing...')
+
+        if 'Acetate' in self._ion_probe:
+            print('It smells like vinegar....')
+        else:
+            print('You smell nothing...')
 
     def test_carbonate(self):
-        pass
+        random_failure = 0
+        print('You have to mix your substance with a couple of drops of HCl 2M.')
+        mg_value = input('How much substance do you use? Mg: ')
+        drop_value = input('How many drops of 2M HCL do you use? Approximate amount of drops: ')
+
+        if int(mg_value) != 10:
+            random_failure += 0.3
+        if not 5 <= int(drop_value) <= 10:
+            random_failure += 0.3
+
+        print('The test tube is quickly sealed with a stopper,  '
+              'which  carries  a  gas  absorptiontube (filled with barium hydroxide solution).')
+
+        if 'Carbonate' not in self._ion_probe or decision(random_failure):
+            return print('Nothing seems to happen...')
+        else:
+            print('The mixture flares up and delivers acolorless and odourless gas.  '
+                  'When slightly heated, a white precipitate is formed which dissolves in excess hydrochloric acid.')
 
     def test_iodide(self):
         pass
