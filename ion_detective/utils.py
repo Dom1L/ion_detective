76ff68a2472cf2b910fb3483c5803155d8854797
Dom1L <lemm.d@web.de>
2020-04-22 09:40:10 +0200

    random fail based on wrong decisions

--- ion_detective/utils.py
+++ ion_detective/utils.py
@@ -1,3 +1,12 @@
+import base64
+import random
+
 anions = ['Acetate', 'Carbonate', 'Iodide', 'Nitrate', 'Phosphate', 'Sulphate']
 cations = ['Aluminum', 'Calcium', 'Copper', 'Iron2', 'Iron3', 'Cobalt', 'Nickle', 'Potassium', 'Sodium']
 
+def _obfuscator(secret, compare):
+    pass
+
+
+def decision(probability):
+    return random.random() < probability
